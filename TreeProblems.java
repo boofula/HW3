3ca525d2c4bcff541701b9dd5e9a924235b4decb
Will Bales <132319268+boofula@users.noreply.github.com>
2025-02-21 22:50:10 -0600

    Update TreeProblems.java

--- TreeProblems.java
+++ TreeProblems.java
@@ -1,5 +1,5 @@
 /*
- * *** YOUR NAME GOES HERE / YOUR SECTION NUMBER ***
+ * *** Will Bales / 002 ***
  *
  * This java file contains several simple tree problems that need to be
  * codified. These routines  must use the TreeMap and TreeSet library
@@ -20,14 +20,22 @@ public class TreeProblems {
    */
   
   public static Set<Integer> different(Set<Integer> setA, Set<Integer> setB) {
+    // Create a new TreeSet to store the symmetric differece of setA and setB
+    Set<Integer> symmetricDiffAB = new TreeSet<Integer>(setA);
+    // Add all elements of setB to symmetricDiffAB
+    symmetricDiffAB.addAll(setB);
 
-    // INSERT CODE HERE - DO NOT FORGET TO PLACE YOUR NAME ABOVE
-    //
-    // This can be done numerous ways, but once such will only that
-    // *several* lines of code. Hint: create two temporary TreeSets and utilize the
-    // methods retainAll(), addAll(), and removeAll(). But in the end, get something to work.
 
-    return setA;
+    // Create a new TreeSet to store the intersection of setA and setB
+    Set<Integer> intersectionAB = new TreeSet<Integer>(setA);
+    // Retain only the elements that are in both setA and setB
+    intersectionAB.retainAll(setB);
+
+    // Removes all elements that are in the intersection to get the symmetric difference of setA and setB
+    symmetricDiffAB.removeAll(intersectionAB);
+
+    // Return the symmetric difference
+    return symmetricDiffAB;
   }
 
 
@@ -40,9 +48,18 @@ public class TreeProblems {
 
   public static void removeEven(Map<Integer, String> treeMap) {
 
-    // INSERT CODE HERE.
+    // Create an iterator to iterate through the keys of the treeMap
+    Iterator<Integer> iteratorForRemoveEvens = treeMap.keySet().iterator();
 
-    return;
+    // Iterate through the keys of the treeMap
+    while (iteratorForRemoveEvens.hasNext()) {
+      // Get the next key
+      Integer key = iteratorForRemoveEvens.next();
+      // If the key is even, remove the <key, value> pair
+      if (key % 2 == 0) {
+        iteratorForRemoveEvens.remove();
+      }
+    }
   }
 
 
@@ -54,11 +71,42 @@ public class TreeProblems {
    */
 
   public boolean treesEqual(Map<Integer, String> tree1,Map<Integer, String> tree2 ) {
+    // Initialize a boolean to store the result of the comparison
+    boolean isTreeEqual = true;
 
-    // INSERT CODE HERE
+    // Initialize a boolean to determine if the comparison should continue
+    boolean continueChecking = true;
 
-    return false;
+    // If the two trees are not the same size, they are not equal and thus isTreeEqual is false
+    if (tree1.size() != tree2.size()) {
+      isTreeEqual = false;
+    } else {
+      // Create an iterator to iterate through the keys of tree1
+      Iterator<Integer> iteratorForTree1 = tree1.keySet().iterator();
 
+      // Iterate through the keys of tree1
+      while (iteratorForTree1.hasNext() && continueChecking) {
+
+        // Get the next key
+        Integer key = iteratorForTree1.next();
+        if (!tree2.containsKey(key)) {
+
+          // If the key is not in tree2, the trees are not equal and thus isTreeEqual is false
+          isTreeEqual = false;
+          continueChecking = false;
+        } else {
+          // If the key is in tree2, compare the values of the two trees
+          if (!tree1.get(key).equals(tree2.get(key))) {
+
+            // If the values are not equal, the trees are not equal and thus isTreeEqual is false
+            isTreeEqual = false;
+            continueChecking = false;
+          }
+        }
+      }
+    }
+
+    return isTreeEqual;
   }
 
 } // end treeProblems class
